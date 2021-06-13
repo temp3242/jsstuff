82a2b8fdb46c40200c002a8ca409d356808123f2
Arthur <arthur3242.go@gmail.com>
2021-06-12 21:51:35 -0300

    Heroku Docker Deploy First Try

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,9 @@
 const nodeExternals = require('webpack-node-externals');
 const webpack = require('webpack')
+const path = require('path')
 
 module.exports = {
+    entry: path.join(__dirname, 'src/index.js'),
     mode: 'production',
     output: {filename:'bundle.js'},
     externals: [nodeExternals()],
